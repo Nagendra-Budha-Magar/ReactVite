8d729e34524a96b25ac774dcb110c1fa4217328c
Ashish_Budha <nagendrabudha47@gmail.com>
2025-05-13 14:13:33 +0545

    improved

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 import { productData } from '../utils/productData';
 const Body = () => {
-  let topRatedProducts = [];
+  const [topRatedProducts, setTopRatedProducts] = useState(productData);
+
   const topRatedProductsData = () => { 
-    productData.filter((product) => product.rating.rate>4);
+    setTopRatedProducts(productData.filter((product) => product.rating.rate>4));
     console.log(topRatedProducts);
   };
   return (
@@ -18,13 +19,13 @@ const Body = () => {
         />
           Search
           </div>
-          <button className='px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2' onClick = {topRatedProductsData}>
+          <button className='px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2' onClick = {topRatedProductsData}>
             Top Rated Products
           </button>
           </div>
 
       <div className='flex flex-wrap justify-center gap-4 product-items'>
-        {productData.map((product) => (
+        {topRatedProducts.map((product) => (
           <ProductCard
             key={product.id}
             image={product.image}
